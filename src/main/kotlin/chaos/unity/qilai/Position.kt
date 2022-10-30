24fef55b74b655458500b4f2896542429f1e2c87
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-10-30 16:54:48 +0800

    add Position

---
+++ src/main/kotlin/chaos/unity/qilai/Position.kt
@@ -0,0 +1,38 @@
+package chaos.unity.qilai
+
+data class Position(
+    val startLine: Int,
+    val startColumn: Int,
+    val endLine: Int,
+    val endColumn: Int,
+    val fileName: String
+) : Comparable<Position> {
+    companion object {
+        val DEFAULT = Position(1, 1, 1 ,1, "<no-file>")
+    }
+
+    override fun toString(): String =
+        "${fileName}@${startLine}:${startColumn}-${endLine}:${endColumn}"
+
+    override fun compareTo(other: Position): Int {
+        if (this.startLine < other.startLine) {
+            return -1
+        }
+        if (this.startLine == other.startLine && this.startColumn < other.startColumn) {
+            return -1
+        }
+        if (this.endLine < other.endLine) {
+            return -1
+        }
+        if (this.endLine == other.endLine && this.endColumn < other.endColumn) {
+            return -1
+        }
+        if (this.startLine == other.startLine && this.startColumn == other.startColumn
+            && this.endLine == other.endLine && this.endColumn == other.endColumn
+        ) {
+            return 0
+        }
+
+        return 1
+    }
+}
